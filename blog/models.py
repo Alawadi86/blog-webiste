07595e07c8c5ec65a442420e24f59fcdb78e4c15
Alawadi86 <malawadi380@gmail.com>
2022-10-01 21:54:13 -0400

    created templates, set up models, urls

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+          """ Post model class  """
+
+          title = models.CharField(max_length=200)
+          author = models.ForeignKey(
+                    "auth.User",
+                    on_delete=models.CASCADE,
+                    related_name="posts",
+                    )
+
+          body = models.TextField()
+          created = models.DateTimeField(auto_now_add=True)
+          updated = models.DateTimeField(auto_now=True)
+
+          def __str__(self):
+                    """ String method """
+                    return self.title
+
+          def get_absolute_url(self):
+                    """Get the absolute Url for the single post """
+                    return reverse("post_detail", kwargs={"pk": self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+
+from .models import Post
+
+class BlogListView(ListView):
+          """Blog List View """
+
+          model = Post
+          template_name = "home.html"
+
+          
+
