494185cb8a7d3dc5da6a0bfd65b7f86dd8f7f137
Alawadi86 <malawadi380@gmail.com>
2022-10-09 18:37:59 -0400

    Add css files via static files,detail view and test for list and detail view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
-
+from .models import Post
 from .models import Post
 
 class BlogListView(ListView):
@@ -10,5 +9,7 @@ class BlogListView(ListView):
           model = Post
           template_name = "home.html"
 
-          
 
+class BlogDetailView(DetailView):
+          model = Post
+          template_name ="post_detail.html"
